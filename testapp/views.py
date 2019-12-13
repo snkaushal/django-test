d02c9d8b648b7943598e346321fd0f81d0da414b
Sahil <skaushal@breadware.com>
2019-12-13 22:22:42 +0530

    added post method in the test api

--- testapp/views.py
+++ testapp/views.py
@@ -1,8 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serilaizers
 
 class TestApiView(APIView):
-
+  serializer_class = serilaizers.TestSerializer
   def get(self, request, format=None):
     test_view = ['testView']
     return Response({'message': 'Hello! this is a test', 'test_view': test_view})
+
+  def post(self, request):
+    serializer = self.serializer_class(data = request.data)
+
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f"Hello {name}"
+      return Response({'message': message})
+    else:
+      return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
