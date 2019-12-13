99f4950cc226dfbecdacb4efac687b27c9b14c54
Sahil <skaushal@breadware.com>
2019-12-13 21:57:21 +0530

    added a test api view

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class TestApiView(APIView):
+
+  def get(self, request, format=None):
+    test_view = ['testView']
+    return Response({'message': 'Hello! this is a test', 'test_view': test_view})
