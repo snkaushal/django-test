ed49218ea191af4c653d819c1fd4bb356adb9dea
Sahil <skaushal@breadware.com>
2019-12-15 01:27:21 +0530

    added viewsets

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register('test-viewset', views.TestViewSet, basename='test-viewset')
+
 urlpatterns = [
-    path('testapi/', views.TestApiView.as_view())
+    path('testapi/', views.TestApiView.as_view()),
+    path('', include(router.urls))
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 from . import serilaizers
 
 class TestApiView(APIView):
@@ -27,3 +28,34 @@ class TestApiView(APIView):
   
   def delete(self, request, pk=None):
       return Response({'method': 'DELETE'})
+
+class TestViewSet(viewsets.ViewSet):
+  serializer_class = serilaizers.TestSerializer
+
+  def list(self, request):
+    a_viewset = [
+      'some string',
+      'another string'
+    ]
+    return Response({'message': 'hello', 'a_viewset': a_viewset})
+  def create(self, request):
+    serializer = self.serializer_class(data = request.data)
+
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f"Hello {name}"
+      return Response({'message': message})
+    else:
+      return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+  def retrieve(self, request, pk=None):
+    return Response({'http_method':'GET'})
+
+  def update(self, request, pk=None):
+    return Response({'http_method':'PUT'})
+
+  def partial_update(self, request, pk=None):
+    return Response({'http_method':'PATCH'})
+
+  def destroy(self, request, pk=None):
+    return Response({'http_method':'DELETE'})
