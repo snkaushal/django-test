90b60070649fd87a8a536e57e117c051be6841f0
Sahil <skaushal@breadware.com>
2019-12-16 15:24:30 +0530

    added serializer and viewset for user profile

--- customuser/views.py
+++ customuser/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from . import serializers
+from . import models
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+  serializer_class = serializers.UserProfileSerializer
+  queryset = models.UserProfile.objects.all()
--- testProject/urls.py
+++ testProject/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('testapp.urls'))
+    path('api/', include('testapp.urls')),
+    path('userapi/', include('customuser.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
