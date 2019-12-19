1078f6e5bb7ca79258c277675557b19b0398d0cf
Sahil <skaushal@breadware.com>
2019-12-19 12:00:01 +0530

    added search filter

--- customuser/views.py
+++ customuser/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from customuser import permissions
 from . import serializers
@@ -11,3 +11,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   queryset = models.UserProfile.objects.all()
   authentication_classes = (TokenAuthentication,)
   permission_classes = (permissions.UpdateOwnProfile,)
+  filter_backends = (filters.SearchFilter,)
+  search_fields = ('name', 'email',)
