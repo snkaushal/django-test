816e113272179df53e485476b31abf538f4effbe
Sahil <skaushal@breadware.com>
2019-12-19 11:55:03 +0530

    added permissions and authentication

--- customuser/views.py
+++ customuser/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from customuser import permissions
 from . import serializers
 from . import models
 # Create your views here.
@@ -7,3 +9,5 @@ from . import models
 class UserProfileViewSet(viewsets.ModelViewSet):
   serializer_class = serializers.UserProfileSerializer
   queryset = models.UserProfile.objects.all()
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (permissions.UpdateOwnProfile,)
