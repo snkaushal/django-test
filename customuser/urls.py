227dffd1c118a39affe49d8ec7ce0f74804e120e
Sahil <skaushal@breadware.com>
2019-12-19 12:13:07 +0530

    added login api

--- customuser/urls.py
+++ customuser/urls.py
@@ -6,5 +6,6 @@ router = DefaultRouter()
 router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- customuser/views.py
+++ customuser/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from customuser import permissions
 from . import serializers
 from . import models
@@ -13,3 +15,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   permission_classes = (permissions.UpdateOwnProfile,)
   filter_backends = (filters.SearchFilter,)
   search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+  renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
